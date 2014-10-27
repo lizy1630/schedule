cf665de5e809c01c800b733b93567b3a635c2e46
Xing Liu <lizy1630@gmail.com>
2014-10-26 21:45:29 -0400

    define exception

---
+++ scheduleAssignment/src/scheduleAssignment/MyExceptions.java
@@ -0,0 +1,11 @@
+package scheduleAssignment;
+
+public class MyExceptions extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public MyExceptions(String msg) {
+		super(msg);
+	}
+
+}
